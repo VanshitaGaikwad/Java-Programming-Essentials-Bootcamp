34a2cfd84084d03396c6f406ba261ef00dbaf946
Vanshita S. Gaikwad <vanshitagaikwad96@gmail.com>
2023-04-01 21:32:27 +0530

    Add files via upload

---
+++ day5.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class GG {
+
+	// Utility function to find sum of
+	// all divisor of number up to 'n'
+	static int divisorSum(int n)
+	{
+		int sum = 0;
+
+		for (int i = 1; i <= n; ++i) {
+
+			// Find all divisors of i
+			// and add them
+			for (int j = 1; j * j <= i; ++j) {
+				if (i % j == 0) {
+					if (i / j == j)
+						sum += j;
+					else
+						sum += j + i / j;
+				}
+			}
+		}
+		return sum;
+	}
+
+	// Driver code
+	public static void main(String args[])
+	{
+		int n = 4;
+		System.out.println(divisorSum(n));
+		n = 5;
+		System.out.println(divisorSum(n));
+	}
+}
